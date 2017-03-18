33abba628d5f265b5e1e68bcd585f6bc2181f8c7
Laura Moreby <lauramoreby@Lauras-MacBook-Pro.local>
2017-03-18 19:33:19 +0000

    CSS added to base style sheet and rearranged base.html

--- foodbookapp/views.py
+++ foodbookapp/views.py
@@ -111,22 +111,22 @@ def register(request):
 														'registered':registered})
 
 def user_login(request):
-	if request.method == 'POST':
-		username = request.POST.get('username')
-		password = request.POST.get('password')
-		user = authenticate(username=username, password=password)
-		if user:
-			if user.is_active: # If the account is valid and active, we log the user in.	
-				login(request, user)
-				return HttpResponseRedirect(reverse('home'))
-			else:
-				# An inactive account was used.
-				return HttpResponse("Your account is disabled.")
-		else: # Bad login details were provided.
-			print("Invalid login details: {0}, {1}".format(username, password))
-			return HttpResponse("Invalid login details supplied.")
-	else:
-		return render(request, 'foodbookapp/login.html', {})
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(username=username, password=password)
+
+        if user:
+        	if user.is_active: # If the account is valid and active, we log the user in.	
+                	login(request, user)
+			return HttpResponseRedirect(reverse('home'))
+		else:
+        		return HttpResponse("Your account is disabled.")
+	else: # Bad login details were provided.
+		print("Invalid login details: {0}, {1}".format(username, password))
+		return HttpResponse("Invalid login details supplied.")
+    else:
+        return render(request, 'foodbookapp/login.html', {})
 
 
 @login_required
