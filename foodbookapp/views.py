b3e34bf5a22307baf6613bf1c800c5b7e6c4c326
Laura Moreby <lauramoreby@Lauras-MacBook-Pro.local>
2017-03-19 19:18:57 +0000

    More CSS added and about page done

--- foodbookapp/views.py
+++ foodbookapp/views.py
@@ -40,7 +40,6 @@ def show_recipe(request, recipe_slug):
 	context_dict = {}
 	try:
 		recipe = Recipe.objects.get(slug = recipe_slug)
-		tags = Tags.objects.get(recipe = recipe)
 		context_dict['recipe'] = recipe
 	except Recipe.DoesNotExist:
 		context_dict['recipe'] = None
