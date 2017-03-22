26e517955695f523d37f09f45a49881e02b321f2
Suritus <matus.mikus@gmail.com>
2017-03-22 22:11:20 +0000

    small update

--- populate_foodbook.py
+++ populate_foodbook.py
@@ -57,7 +57,7 @@ def add_recipe(title, views, recipeText, pictureLink, comments):
 	r.pictureLink = pictureLink
 	r.save()
 	for comment in comments:
-		user = User.objects.get_or_create(username = "admin", password = "foodbook")
+		user = User.objects.get_or_create(username = "admin", password = "foodbook")[0]
 		user.save()
 		c = Comment.objects.create(body = comment, user = user, recipe = r)
 	return r
